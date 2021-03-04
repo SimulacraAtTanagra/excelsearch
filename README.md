af549391b61ed90fec026fe435723849f7c5d086
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-03-03 21:23:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## The purpose of this project is as follows:
+Allows for searching the contents of excel files to see if they contain a given term.
+## Here's some back story on why I needed to build this:
+I deal with a ton of data and sometimes remembering what I named a given file that I need, with 8 years of files on my work harddrive, isn't very easy. If the data is sufficiently unique, this allows me to find files slightly easier than the alternative method. 
+This project leverages pandas.
+
+
+The expected frequency for running this code is As Needed.
---
+++ excelsearch.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import os
+
+
+def filesearch(workbookName,particularString):
+    dfwork = pd.read_excel(workbookName)
+    if particularString in dfwork.values:
+        print(workbookName)
+        
+def main(directory_in_str,term):
+    directory = os.fsencode(directory_in_str)           #defines directory as indicated string
+    os.chdir(directory)                                 #navigate to directory specified
+    for file in os.listdir(directory):                  #iterates over all the files here
+        filename = os.fsdecode(file)                    #specifies filename from file
+        if filename.endswith('xlsx') or filename.endswith('xls'):
+            fullpath =f'{directory_in_str}\\{filename}'
+            try:
+                filesearch(fullpath,term)
+            except:
+                print(f'Search failed for {filename}')
+                continue
+    
